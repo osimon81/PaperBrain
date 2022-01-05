87d54701b8e38dacd7832f9153c7d88e630cfdfb
Simon Ogundare <osimon81@gmail.com>
2022-01-05 02:05:32 +0100

    Markdown v4.0

--- Scripts/Complex_Diagramming.R
+++ Scripts/Complex_Diagramming.R
@@ -3,13 +3,17 @@
 
 instructions <- function() {
   print("Hello! Welcome to version 1 of the concerted MicroNetwork construction interface.")
-  print("This project is essentially a pipeline which allows for fluid force-directed network construction.")
+  print(
+    "This project is essentially a pipeline which allows for fluid force-directed network construction."
+  )
   print("To create MicroNetworks, use the following lines as syntax examples:")
-  print("AND(n,n): create an AND MicroNetwork with any number of input neurons n and any output neurons n.")
-  print("NOT(3, 1): create a NOT MicroNetwork with 3 input neurons and 1 output neuron")
+  print(
+    "AND(n,n): create an AND MicroNetwork with any number of input neurons n and any output neurons n."
+  )
+  print("NOT(3, 1): create a NOT MicroNetwork with 3 input neurons and 1 output neuron.")
 }
 
-constructLinearInterface <- function() {
+constructLinearMicroNetwork <- function() {
   instructions()
   circuitList <- c()
   
@@ -19,8 +23,8 @@ constructLinearInterface <- function() {
   id <- 1
   editMode <- TRUE
   while (editMode == TRUE) {
-    
-    selectedNet <- readline(prompt = "Name the MicroNetwork you would like to add: ")
+    selectedNet <-
+      readline(prompt = "Name the MicroNetwork you would like to add: ")
     if (grepl("AND", selectedNet) == TRUE) {
       circuitList <- c(circuitList, selectedNet)
       identifier <- as.character(id)
@@ -30,62 +34,78 @@ constructLinearInterface <- function() {
       outputs <- as.numeric(parse_number(in_out_count[[1]][2]))
       print("MicroNetwork Type: And") # Unique feedback for AND MicroNetworks
       print(paste("MicroNetwork Identifier:", identifier))
-      print(paste("MicroNetwork", identifier, "Inputs:", as.character(inputs)))
-      print(paste("MicroNetwork", identifier, "Outputs:", as.character(outputs)))
+      print(paste(
+        "MicroNetwork",
+        identifier,
+        "Inputs:",
+        as.character(inputs)
+      ))
+      print(paste(
+        "MicroNetwork",
+        identifier,
+        "Outputs:",
+        as.character(outputs)
+      ))
       
       # Assemble AND MicroNetwork neurons here (for the time being, this network only makes computational sense when input number is set to 3)
       
-        src <- c(src,
-                 
-                 paste0(identifier, "(1a)+"),
-                 paste0(identifier, "(2a)+"),
-                 paste0(identifier, "(3a)+"),
-                 paste0(identifier, "(1a)+"),
-                 paste0(identifier, "(2a)+"),
-                 paste0(identifier, "(3a)+"),
-                 paste0(identifier, "(4a)+"),
-                 paste0(identifier, "(4a)+"),
-                 paste0(identifier, "(6a)-"),
-                 paste0(identifier, "(7a)-"))
+      src <- c(
+        src,
         
+        paste0(identifier, "(1a)+"),
+        paste0(identifier, "(2a)+"),
+        paste0(identifier, "(3a)+"),
+        paste0(identifier, "(1a)+"),
+        paste0(identifier, "(2a)+"),
+        paste0(identifier, "(3a)+"),
+        paste0(identifier, "(4a)+"),
+        paste0(identifier, "(4a)+"),
+        paste0(identifier, "(6a)-"),
+        paste0(identifier, "(7a)-")
+      )
+      
+      
+      target <- c(
+        target,
+        paste0(identifier, "(4a)+"),
+        paste0(identifier, "(4a)+"),
+        paste0(identifier, "(4a)+"),
+        paste0(identifier, "(5a)+"),
+        paste0(identifier, "(5a)+"),
+        paste0(identifier, "(5a)+"),
+        paste0(identifier, "(6a)-"),
+        paste0(identifier, "(7a)-"),
+        paste0(identifier, "(5a)+"),
+        paste0(identifier, "(5a)+")
+      )
+      
+      if (as.numeric(identifier) > 1) {
+        src <- c(src,
+                 paste0(as.character((
+                   as.numeric(identifier) - 1
+                 )), "(Out1)"))
         
         target <- c(target,
-                    paste0(identifier, "(4a)+"),
-                    paste0(identifier, "(4a)+"),
-                    paste0(identifier, "(4a)+"),
-                    paste0(identifier, "(5a)+"),
-                    paste0(identifier, "(5a)+"),
-                    paste0(identifier, "(5a)+"),
-                    paste0(identifier, "(6a)-"),
-                    paste0(identifier, "(7a)-"),
-                    paste0(identifier, "(5a)+"),
-                    paste0(identifier, "(5a)+"))
-        
-        if (as.numeric(identifier) > 1) {
-          src <- c(src,
-                   paste0(as.character((as.numeric(identifier)-1)), "(Out1)"))
-          
-          target <- c(target,
-                      paste0(identifier, "(In1)"))
-        }
-        
-        
-        for (i in 1:inputs) {
-          src <- c(src,
-                    paste0(identifier, "(In", as.character(i), ")"))
-          
-          target <- c(target,
-                      paste0(identifier, "(", i, "a)+"))
-         }
-        
-        for (i in 1:outputs) {
-          src <- c(src,
-                   paste0(identifier, "(5a)+"))
-          
-          target <- c(target,
-                      paste0(identifier, "(Out", as.character(i), ")"))
-        }
+                    paste0(identifier, "(In1)"))
       }
+      
+      
+      for (i in 1:inputs) {
+        src <- c(src,
+                 paste0(identifier, "(In", as.character(i), ")"))
+        
+        target <- c(target,
+                    paste0(identifier, "(", i, "a)+"))
+      }
+      
+      for (i in 1:outputs) {
+        src <- c(src,
+                 paste0(identifier, "(5a)+"))
+        
+        target <- c(target,
+                    paste0(identifier, "(Out", as.character(i), ")"))
+      }
+    }
     else if (grepl("LOOP", selectedNet) == TRUE) {
       circuitList <- c(circuitList, selectedNet)
       identifier <- as.character(id)
@@ -100,42 +120,48 @@ constructLinearInterface <- function() {
       
       # Assemble LOOP MicroNetwork neurons here:
       
+      src <- c(
+        src,
+        paste0(identifier, "(1l)+"),
+        paste0(identifier, "(2l)+"),
+        paste0(identifier, "(3l)+"),
+        paste0(identifier, "(4l)+")
+      )
+      
+      target <- c(
+        target,
+        paste0(identifier, "(2l)+"),
+        paste0(identifier, "(3l)+"),
+        paste0(identifier, "(4l)+"),
+        paste0(identifier, "(1l)+")
+      )
+      
+      if (as.numeric(identifier) > 1) {
         src <- c(src,
-                 paste0(identifier, "(1l)+"),
-                 paste0(identifier, "(2l)+"),
-                 paste0(identifier, "(3l)+"),
-                 paste0(identifier, "(4l)+"))
+                 paste0(as.character((
+                   as.numeric(identifier) - 1
+                 )), "(Out", as.character(i), ")"))
         
         target <- c(target,
-                    paste0(identifier, "(2l)+"),
-                    paste0(identifier, "(3l)+"),
-                    paste0(identifier, "(4l)+"),
-                    paste0(identifier, "(1l)+"))
-        
-        if(as.numeric(identifier) > 1) {
-          src <- c(src,
-                   paste0(as.character((as.numeric(identifier)-1)), "(Out", as.character(i), ")"))
-          
-          target <- c(target,
-                      paste0(identifier, "(In1)"))
-        }
-
-        for (i in 1:inputs) {
-          src <- c(src,
-                   paste0(identifier, "(In", as.character(i), ")"))
-          
-          target <- c(target,
-                      paste0(identifier, "(1l)+"))
-        }
-        
-        for (i in 1:outputs) {
-          src <- c(src,
-                   paste0(identifier, "(3l)+"))
-          
-          target <- c(target,
-                      paste0(identifier, "(Out", as.character(i), ")"))
-        }
+                    paste0(identifier, "(In1)"))
       }
+      
+      for (i in 1:inputs) {
+        src <- c(src,
+                 paste0(identifier, "(In", as.character(i), ")"))
+        
+        target <- c(target,
+                    paste0(identifier, "(1l)+"))
+      }
+      
+      for (i in 1:outputs) {
+        src <- c(src,
+                 paste0(identifier, "(3l)+"))
+        
+        target <- c(target,
+                    paste0(identifier, "(Out", as.character(i), ")"))
+      }
+    }
     else if (grepl("NOT", selectedNet) == TRUE) {
       circuitList <- c(circuitList, selectedNet)
       identifier <- as.character(id)
@@ -150,15 +176,17 @@ constructLinearInterface <- function() {
       
       # Assemble NOT MicroNetwork neurons here:
       
-        src <- c(src,
-                 paste0(identifier, "(1n)-"))
-        
-        target <- c(target,
-                    paste0(identifier, "(2n)+"))
+      src <- c(src,
+               paste0(identifier, "(1n)-"))
+      
+      target <- c(target,
+                  paste0(identifier, "(2n)+"))
       
       if (as.numeric(identifier) > 1) {
         src <- c(src,
-                 paste0(as.character((as.numeric(identifier)-1)), "(Out", as.character(i), ")"))
+                 paste0(as.character((
+                   as.numeric(identifier) - 1
+                 )), "(Out", as.character(i), ")"))
         
         target <- c(target,
                     paste0(identifier, "(In1)"))
@@ -184,8 +212,9 @@ constructLinearInterface <- function() {
       print("You have entered an invalid MicroNetwork type.")
       id <- id - 1
     }
-  
-    continue <- readline(prompt = "Would you like to add a new MicroNetwork? (Y/N) ")
+    
+    continue <-
+      readline(prompt = "Would you like to add a new MicroNetwork? (Y/N) ")
     
     if (toupper(continue) == "Y") {
       editMode <- TRUE
@@ -200,13 +229,108 @@ constructLinearInterface <- function() {
   }
   
   networkDF <- data.frame(src, target)
-  simpleNetwork(networkDF, fontSize = 14,
-                linkDistance = 100, zoom = TRUE)
+  simpleNetwork(
+    networkDF,
+    fontSize = 14,
+    linkDistance = 100,
+    zoom = TRUE
+  )
 }
 
 
-
-
 interpretNeuron <- function(id) {
-  neuronAttributes <- list()
+  if (grepl("Out", id) == TRUE) {
+    neuronAttributes <- list(
+      "Neuron Type" = "Output Neuron (sensory)",
+      "Activity" = "Excitatory",
+      "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                           1),
+      "Subpathway Identifier" = sub(".*Out", "", substr(id, 1, nchar(id) -
+                                                          1))
+    )
+  }
+  else if (grepl("In", id) == TRUE) {
+    neuronAttributes <- list(
+      "Neuron Type" = "Input Neuron (sensory)",
+      "Activity" = "Excitatory",
+      "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                           1),
+      "Subpathway Identifier" = sub(".*In", "", substr(id, 1, nchar(id) -
+                                                         1))
+    )
+  }
+  else if (grepl("a)", id) == TRUE) {
+    neuronAttributes <-
+      list("Neuron Type" = "Interneuron (AND MicroNetwork)")
+    if (grepl("-|[\U{2013}]", id) == TRUE) {
+      neuronAttributes <- c(
+        neuronAttributes,
+        "Activity" = "Inhibitory",
+        "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                             1),
+        "Subpathway Identifier" = as.character(parse_number(substr(
+          id, 2, nchar(id)
+        )))
+      )
+    }
+    else if (grepl(as.character("+"), id) == TRUE) {
+      neuronAttributes <- c(
+        neuronAttributes,
+        "Activity" = "Excitatory",
+        "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                             1),
+        "Subpathway Identifier" = as.character(parse_number(substr(id, 2, nchar(id))))
+      )
+    }
+  }
+  else if (grepl("l)", id) == TRUE) {
+    neuronAttributes <-
+      list("Neuron Type" = "Interneuron (LOOP MicroNetwork)")
+    if (grepl("-|[\U{2013}]", id) == TRUE) {
+      neuronAttributes <- c(
+        neuronAttributes,
+        "Activity" = "Inhibitory",
+        "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                             1),
+        "Subpathway Identifier" = as.character(parse_number(substr(
+          id, 2, nchar(id)
+        )))
+      )
+    }
+    else if (grepl(as.character("+"), id) == TRUE) {
+      neuronAttributes <- c(
+        neuronAttributes,
+        "Activity" = "Excitatory",
+        "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                             1),
+        "Subpathway Identifier" = as.character(parse_number(substr(id, 2, nchar(id))))
+      )
+    }
+  }
+  else if (grepl("n)", id) == TRUE) {
+    neuronAttributes <-
+      list("Neuron Type" = "Interneuron (NOT MicroNetwork)")
+    if (grepl("-|[\U{2013}]", id) == TRUE) {
+      neuronAttributes <- c(
+        neuronAttributes,
+        "Activity" = "Inhibitory",
+        "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                             1),
+        "Subpathway Identifier" = as.character(parse_number(substr(
+          id, 2, nchar(id)
+        )))
+      )
+    }
+    else if (grepl(as.character("+"), id) == TRUE) {
+      neuronAttributes <- c(
+        neuronAttributes,
+        "Activity" = "Excitatory",
+        "MicroNetwork Identifier" = substr(id, 1, regexpr("(", id, fixed = TRUE) -
+                                             1),
+        "Subpathway Identifier" = as.character(parse_number(substr(id, 2, nchar(id))))
+      )
+    }
+  }
+  
+  str(neuronAttributes)
 }
